8aba4b9d5a9b0f21956c76aac5fb6421e7356ca2
YatharthDadu <yatharth.dadu@gmail.com>
2025-11-28 21:03:22 -0600

    This function uses a Taylor Series Formula to approximate cosine to a very close value of cosine.

--- PlaygroundActivities.playground/Pages/CharacterRectangle.xcplaygroundpage/Contents.swift
+++ PlaygroundActivities.playground/Pages/CharacterRectangle.xcplaygroundpage/Contents.swift
@@ -1,7 +1,43 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+func repChar(n: Int, ch: String) -> String
+{
+    var asteriskMaker = ""
+    for i in 1...n
+    {
+        asteriskMaker += ch
+    }
+    return asteriskMaker
+}
 
-//: [Next](@next)
+print(repChar(n: 5, ch: "#"))
+func rectCharStroke(n: Int, ch: String = "*") -> String
+{
+    var asteriskMaker = ""
+    if (n < 0)
+    {
+        return "Invalid Number!"
+    }
+    else if(n == 1)
+    {
+        return "*"
+    }
+    
+    else if(n == 2)
+    {
+        return "** \n**"
+    }
+    else
+    {
+        asteriskMaker += "\(repChar(n: n, ch: ch))\n*"
+        for i in 1..<n-1
+        {
+            asteriskMaker += " "
+        }
+        asteriskMaker += "*\n"
+        asteriskMaker += "\(repChar(n:n, ch:ch))"
+        
+    }
+    return asteriskMaker
+}
+print(rectCharStroke(n: 3))
--- PlaygroundActivities.playground/Pages/CosineFunction.xcplaygroundpage/Contents.swift
+++ PlaygroundActivities.playground/Pages/CosineFunction.xcplaygroundpage/Contents.swift
@@ -1,7 +1,50 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+func factorialFunction(n: Int) -> Double
+{
+    if n == 0
+    {
+        return 1
+    }
+    var result = 1.0
+    for i in Int(1.0)...n
+    {
+        result *= Double(i)
+    }
+    
+    return result
+}
 
-//: [Next](@next)
+func myCosine(_ inputX: Double, threshold: Double = pow(10,-6)) -> Double
+{
+    var cosFunc = 1 - (pow(inputX,Double(2))/factorialFunction(n: 2))
+    var n = 2;
+    var loopCount = 0;
+    var currentTerm = 1.0
+    while currentTerm > abs(threshold)
+    {
+        currentTerm = pow(inputX, Double(n+2))/factorialFunction(n: n+2)
+        if(loopCount % 2 == 0)
+        {
+            cosFunc += currentTerm
+            n += 2
+            loopCount += 1
+        }
+        else if (loopCount % 2 != 0)
+        {
+            cosFunc -= currentTerm
+            n += 2
+            loopCount += 1
+        }
+    }
+    return cosFunc
+}
+
+let m = 18.0
+for i in stride(from:0, to: m, by:1)
+{
+    let angle: Double = (2 * Double.pi * i) / m
+    print(cos(angle))
+    print(myCosine(angle))
+    print("")
+}
